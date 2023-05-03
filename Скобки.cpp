a46c660f76125b59766766639c758936344a2d95
MrMurder4all <64412699+MrMurder4all@users.noreply.github.com>
2023-05-03 10:34:42 +0300

    Добавьте файлы проекта.

---
+++ Скобки.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+#include <fstream>
+#include <stack>
+#include <string>
+
+using namespace std;
+
+
+
+int main() {
+	ifstream InputFile("input.txt");
+	InputFile.close();
+	return 0;
+}
